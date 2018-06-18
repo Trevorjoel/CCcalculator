92fb6f214408f361df53d0cd8a2463113f5970fb
Trevorjoel <trevsstuff@hotmail.com>
2018-06-18 16:30:52 +1000

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Trevorjoel <trevsstuff@hotmail.com>
    
    # Conflicts:
    #	practice_calcjs.js

--- calcjs.js
+++ calcjs.js
@@ -1,15 +1,29 @@
-var priceArray =[];
+/* What's next?
+    I haven't worked on it for a while. I just cleaned and commented the code. I need to go over it again.
+    But what the app should do next, after listing the currencies and listing the movement over the given periods.
+    The user enters the amount of each currency they own and click enter. A new row (under the currency) appears that displays the
+    price in USD that they have lost/gained over the periods for that currency. They can do this for as many currencies as they like.
+    At the bottom will be a totals row that will add each USD amount and show the user the total USD value lost or gained across all crypto-currencies entered.
+
+*/
+// Initialise variables
+// Keep track of calls to call coin function (how many new nodes created)
 var timesCalled = 0;
+
+var oneHrArray = [];
+var completeArray = [];
+
+// Hack to add header to API response. I keep getting a CORS error due to no response. It's a public API
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 var url = 'https://api.coinmarketcap.com/v1/ticker/';
+
 const addRow = document.getElementById('new_row');
 var cryptoContainer = document.getElementById('crypto-container');
-var catchThing = document.getElementById('row_'+ timesCalled + '"');
-var bitCoin = document.getElementById("bitcoin");
 var valueInFiat ='';
-var newRows = "";
-var newRowEnd = "";
 
+
+// Currency icons
+var bitCoin = document.getElementById("bitcoin");
 bitCoin.addEventListener('click', function () {
 url = url + 'bitcoin';
     callCoin();
@@ -38,8 +52,9 @@ ripple.addEventListener('click', function () {
     ripple.classList.add("hide-me");
 });
 
-
+// Get JSON, Render HTML (needs promises)
 function callCoin(){
+
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', proxyurl + url);
 
@@ -48,113 +63,57 @@ function callCoin(){
 
         renderHTML(ourData);
 
-
-        createArray(ourData);
+       // createArray(ourData);
         changeRedGreen(ourData, timesCalled);
 
         url = ('https://api.coinmarketcap.com/v1/ticker/');
         timesCalled++;
+
     };
     ourRequest.send()
 
 }
-
-
+// Following function needs a more elegant solution, handlebars does not seem to have a way to render entire new HTML nodes
+// there is new templating for ES6 using `` to encapsulate the string and ${variable} to insert vars
+// Renders a new row
 function renderHTML(data) {
-
 
     var htmlString = "";
     for(i = 0; i < data.length; i++){
-        htmlString += "<tr>" +
-            "<td class='box box-name'>"+ data[i].name +"<br /><img class='icons' src='./icons/" + data[i].id + ".png'></td>" +
+        htmlString += "<tr id='row-id_"+ timesCalled +"'>" + "'<td class='box box-name'>"+ data[i].name +"<br /><img class='icons' src='./icons/" + data[i].id + ".png'></td>" +
             "<td class='box box-name'>" + data[i].price_usd +"</td>" +
             "<td id='percent_change_1h' class='box box-name percent_change_1h"+ timesCalled + "'>" + data[i].percent_change_1h +"</td>" +
             "<td id='percent_change_24h' class='box box-name percent_change_24h"+ timesCalled +"'>" + data[i].percent_change_24h +"</td>" +
             "<td id='percent_change_7d' class='box box-name percent_change_7d"+ timesCalled + "'>" + data[i].percent_change_7d +"</td>" +
             "<td class='input-amount'><input    type=\"text\" name=\"amount\" id='submit-amount'></td>" +
-            "<td class='box box-name' >-</td></tr>";
-
-            /*"<div id='' class='flex-dir-column catch-me box small-11 large-3 medium-5 columns'><div class='box box-name'><img class='icons' src='./icons/" + data[i].id + ".png'> </br>" + data[i].name + "<br> " +
-            "" + data[i].price_usd + " USD</div><div class='box'> 1 hour : </div>" +
-            "<div id='percent_change_1h' class='box percent_change_1h"+ timesCalled + "'>"
-            + data[i].percent_change_1h + " % </div><div> 24 hours :</div> " +
-            "<div id='percent_change_24h' class='box percent_change_24h"+ timesCalled + "'>"
-            + data[i].percent_change_24h + " %</div><div> 7 days:</div> " +
-            "<div id='percent_change_7d' class='box percent_change_7d"+ timesCalled + "'>"
-            + data[i].percent_change_7d + " %</div> <form name='quantity' method='get'>\n" +
-
-            "  <label class='box'>Enter the amount of " + data[i].name + " you own? :</label>" +
-            "        <input type=\"text\" name=\"amount\" id='submit-amount'><br>\n" +
-            "\n" +
-            "        <input id='calculate1' type=\"button\" value=\"Submit\" onclick='calcTotalVal()'>\n" +
-            "    </form></div>";
-*/
+            "<td class='box box-name' ><form>\n" +
+            "<input id='calculate1' type=\"button\" value=\"Enter\" onclick='calcTotalVal(completeArray)'>\n" +
+            "</form></td></tr>";
 
     }
 
-    addRow.insertAdjacentHTML('beforeBegin',htmlString);
-
+    addRow.insertAdjacentHTML('afterEnd',htmlString);
 
     return htmlString;
 
 
-
 }
-/*
-function addRows() {
-    var elems = document.getElementsByClassName('catch-me');
-    for (var i = 0, max=elems.length + 1; i < max; i++) {
 
-        if(max % 3 === 0 || timesCalled === 0) {
-            //console.log(max);
-
-                var beginRow = '<div class="row" id="row_'+ timesCalled + '">';
-                var endRow = '</div>';
-                console.log('hit');
-                var cunt = [beginRow,endRow];
-
-
-            break;
-
-        }else{
-
-            console.log(max);
-            console.log('miss');
-            break;
-        }
-
-    }
-   // return cunt;
-}
-*/
-
-var oneHrArray = [];
-var completeArray = [];
-
+// Creates an array of all of the coins called.. Later to be used when calculating combined values.
 function createArray(data) {
     for(i = 0; i < data.length; i++){
-        completeArray.push(data[0]);
-        return completeArray;
+        // completeArray.push(data[0]);
+        //console.log(completeArray);
+       return completeArray;
 
     }
 
 }
-//Math for this again price of CC * price USD
-function calcTotalVal(data){
-    var valueInfiat = 0;
-    var valueInCC = document.getElementById("submit-amount").value;
-    //valueInFiat = valueInCC * completeArray.price;
-    for (i = 0; valueInfiat.length; i++){
-        console.log(data);
-    }
 
-
-
-
-}
+// Change response numbers to red or green if positive or negative int's
 function changeRedGreen(data) {
-
-    if (data[i].percent_change_24h[0] === "-") {
+// I was using data[i] below ?
+    if (data[0].percent_change_24h[0] === "-") {
 
         var addRed = document.querySelectorAll(".percent_change_24h"+timesCalled);
 
@@ -172,7 +131,7 @@ function changeRedGreen(data) {
         }
     }
 
-    if (data[i].percent_change_1h[0] === "-") {
+    if (data[0].percent_change_1h[0] === "-") {
         var addRed = document.querySelectorAll(".percent_change_1h"+timesCalled);
 
         for (var ii = 0; ii < addRed.length; ii++) {
@@ -187,7 +146,7 @@ function changeRedGreen(data) {
             addGreen[ii].classList.add('green');
         }
     }
-    if (data[i].percent_change_7d[0] === "-") {
+    if (data[0].percent_change_7d[0] === "-") {
         var addRed = document.querySelectorAll(".percent_change_7d"+timesCalled);
 
         for (var ii = 0; ii < addRed.length; ii++) {
@@ -204,5 +163,14 @@ function changeRedGreen(data) {
     }
 }
 
+/* IGNORE THIS
+
+   var valueInfiat = 0;
+   var valueInCC = document.getElementById("submit-amount").value;
+   //valueInFiat = valueInCC * completeArray.price;
+   for (i = 0; valueInfiat.length; i++){
+       console.log(data);
+   }
+*/
 
 //header("Access-Control-Allow-Origin: *");
